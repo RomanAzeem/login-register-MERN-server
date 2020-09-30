e5274d1bb53e8e7b254957a8297f9520fb18e75e
Roman <romanazeem007@gmail.com>
2020-09-30 16:01:18 +0500

    update server configuration

--- server.js
+++ server.js
@@ -1,3 +1,49 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+///Load routes from routes directory
+const users = require('./routes/users');
+
 const app = express();
-app.listen(3000, console.log('Server is running'));
+
+//Body Parser
+app.use(express.json());
+
+//// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Mount Routes
+app.use('/api/users', users);
+
+//Not Found Error Handler
+app.use((req, res, next) => {
+  return next(
+    res.status(404).json({
+      success: false,
+      message: 'Not Found',
+    })
+  );
+});
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () =>
+  console.log(`Server is runnig on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
